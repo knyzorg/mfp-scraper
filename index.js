0d9afff9cd1e3158dc4275b01624b875de3daddf
Slava Knyazev <slava@knyz.org>
2017-05-23 20:30:14 -0400

    removed proxy

--- index.js
+++ index.js
@@ -8,23 +8,21 @@ var limiter = new RateLimiter(1, 500);
 Array.max = function( array ){
     return Math.max.apply( Math, array );
 };
-var code = fs.readdirSync("enum").map(Number).sort(function sortNumber(a,b) {
-    return a - b;
-}).reverse()[0];
+var code = fs.readdirSync("enum").map(Number).sort(function sortNumber(a,b) {return a - b;}).reverse()[0];
 console.log(code);
 
 
 function test(code) {
 
-    var options = {
+   /* var options = {
         /*host: "45.32.231.36",
-        port: 31280,*/
-        path: 'http://www.myfitnesspal.com/food/calories/' + code,
+        port: 31280,
+        path: ,
         headers: {
             Host: "www.myfitnesspal.com"
         }
-    };
-    http.get(options, function (res) {
+};*/
+    http.get('http://www.myfitnesspal.com/food/calories/' + code, function (res) {
         console.log(code, res.statusCode)
         if (res.statusCode == 200) {
             fs.writeFile("enum/" + code, "", () => { })
